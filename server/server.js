caec8ec52f0834d502ae75232fc227139c38e79a
Peyton Schlafley <pschlafley0@gmail.com>
2021-01-12 20:29:39 -0500

    added user authenticatoin with mutations and the ability to add thoughts and reactions and friends

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { authMiddleware } = require('./utils/auth');
 
 // import Apollo Server
 const { ApolloServer } = require('apollo-server-express');
@@ -14,7 +15,8 @@ const app = express();
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 // integrate our Apollo server with the Express application as middleware
